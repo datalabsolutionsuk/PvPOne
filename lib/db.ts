237af30795289beecb1e2c5f372746c4e310760b
datachampionuk <datachampion.uk@gmail.com>
2025-12-21 17:48:34 +0000

    Prioritize POSTGRES_URL over DATABASE_URL

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "@/db/schema";
 
-const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_URL!;
+const connectionString = process.env.POSTGRES_URL || process.env.DATABASE_URL!;
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false });
